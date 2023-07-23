54255bebc062539e1f6a5009cf14f567d3fbb2b3
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-23 15:55:24 -0400

    Criando repositorio do projeto

---
+++ conversordemoedas/src/br/com/challengesoneoraclenexteducationalura/conversordemoedas/TesteConversorDeMoedas.java
@@ -0,0 +1,10 @@
+package br.com.challengesoneoraclenexteducationalura.conversordemoedas;
+
+public class TesteConversorDeMoedas {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
