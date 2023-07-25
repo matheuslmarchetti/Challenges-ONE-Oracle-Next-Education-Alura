58e01b267fe1e3f9b96229bfb72f755ef4dd6103
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-25 11:14:14 -0400

    Desenvolvimento da caixa de dialogo do conversor de moedas

--- conversordemoedas/src/br/com/challengesoneoraclenexteducationalura/conversordemoedas/modelos/ConversorDeMoedas.java
+++ conversordemoedas/src/br/com/challengesoneoraclenexteducationalura/conversordemoedas/modelos/ConversorDeMoedas.java
@@ -3,11 +3,41 @@ package br.com.challengesoneoraclenexteducationalura.conversordemoedas.modelos;
 import javax.swing.JOptionPane;
 
 public class ConversorDeMoedas {
+	
+	private double valorConvertidoParaDouble;
+	private String entradaDeValorParaConversao;
 
 	public ConversorDeMoedas() {
 		
-		JOptionPane.showMessageDialog(null, "Bem-vindo(a)!", "Conversor de Moedas", 
-				JOptionPane.INFORMATION_MESSAGE);
+		entradaDeValorParaConversao = JOptionPane.showInputDialog(null, "Bem-vindo(a)!" + 
+				System.lineSeparator() + "Insira um valor a ser convertido: ", 
+						"Conversor de Moedas", JOptionPane.QUESTION_MESSAGE);	
+		
+		LeituraDeDados();
+
+	}
+	
+	public void LeituraDeDados() {
+		
+		try {
+			valorConvertidoParaDouble = Double.parseDouble(entradaDeValorParaConversao);
+			if (valorConvertidoParaDouble >= 0) {
+				JOptionPane.showMessageDialog(null, "Valor informado: " + valorConvertidoParaDouble, "Conversor de Moedas", 
+						JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Informe um valor maior que zero!" , "Atenção!", 
+						JOptionPane.WARNING_MESSAGE);
+				new ConversorDeMoedas();
+			}
+		} catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Voltando ao Menu Inicial...", "Informação", 
+					JOptionPane.INFORMATION_MESSAGE);
+					new ConversorMenuPrincipal();
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Digite apenas números!" , "Erro!", 
+					JOptionPane.ERROR_MESSAGE);
+			new ConversorDeMoedas();
+		}
 		
 	}
 	
