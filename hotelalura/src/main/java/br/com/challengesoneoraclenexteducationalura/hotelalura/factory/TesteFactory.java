d4ad3082d2d565a496393eb9ad7a680ee7f9d9f8
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-09-28 09:15:31 -0400

    Criando repositório challenge hotel alura

---
+++ hotelalura/src/main/java/br/com/challengesoneoraclenexteducationalura/hotelalura/factory/TesteFactory.java
@@ -0,0 +1,7 @@
+package br.com.challengesoneoraclenexteducationalura.hotelalura.factory;
+
+public class TesteFactory {
+    public static void main(String[] args) {
+        System.out.println("Teste Factory!");
+    }
+}
